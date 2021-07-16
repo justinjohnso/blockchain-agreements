4b1fc4297d304ee001a410cdb239d657c334b39f
Andrew DaRe <andrewdare@dropbox.com>
2021-07-16 10:51:32 -0700

    Fixed ESlint errors

--- dappreact/components/signatureRequests.tsx
+++ dappreact/components/signatureRequests.tsx
@@ -7,13 +7,13 @@ export default function SignatureRequests({signatureRequests, signAgreement, isS
     function renderSignatures() {
         return signatureRequests.map((request: any) => (
             request.isSigned ? 
-            <div className={styles.card}>
+            <div className={styles.card} key={request.id}>
                 <h2>{request.name} <em>(SIGNED)</em></h2>
                 <span><b>Sent by:</b><br />{request.owner}</span><br/>
                 <span><b>Copy:</b><br />{request.content}</span>
             </div>
         :
-            <a onClick={() => signAgreement(web3, request.id)} className={styles.card}>
+            <a onClick={() => signAgreement(web3, request.id)} className={styles.card} key={request.id}>
                 <h2>{request.name} &rarr;</h2>
                 <span ><b>Sent by:</b><br />{request.owner}</span><br/>
                 <span><b>Copy:</b><br />{request.content}</span>
@@ -23,7 +23,7 @@ export default function SignatureRequests({signatureRequests, signAgreement, isS
 
     function renderSentSignatures() {
         return signatureRequests.map((request: any) => (
-            <div className={styles.card}>
+            <div className={styles.card} key={request.id}>
                 <h2>{request.name} {request.isSigned && <em>(SIGNED)</em>}</h2>
                 <span><b>Sent to:</b><br />{request.signer}</span><br/>
                 <span><b>Copy:</b><br />{request.content}</span>
