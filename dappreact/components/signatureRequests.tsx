77e9d6dad1f5a43c5a01fb05f754fb7c796f1f93
Andrew DaRe <andrewdare@dropbox.com>
2021-07-16 10:48:47 -0700

    Added sent requests page

--- dappreact/components/signatureRequests.tsx
+++ dappreact/components/signatureRequests.tsx
@@ -1,26 +1,38 @@
 import styles from '../styles/Home.module.css';
 import { useAccount } from '../hooks/useAccount';
 
-export default function SignatureRequests({signatureRequests, signAgreement}: any) {
+export default function SignatureRequests({signatureRequests, signAgreement, isSent}: any) {
     const web3 = useAccount();
 
     function renderSignatures() {
         return signatureRequests.map((request: any) => (
             request.isSigned ? 
             <div className={styles.card}>
-                <h2>{request.name} (SIGNED)</h2>
-                <p>{request.content}</p>
+                <h2>{request.name} <em>(SIGNED)</em></h2>
+                <span><b>Sent by:</b><br />{request.owner}</span><br/>
+                <span><b>Copy:</b><br />{request.content}</span>
             </div>
         :
             <a onClick={() => signAgreement(web3, request.id)} className={styles.card}>
                 <h2>{request.name} &rarr;</h2>
-                <p>{request.content}</p>
+                <span ><b>Sent by:</b><br />{request.owner}</span><br/>
+                <span><b>Copy:</b><br />{request.content}</span>
             </a>
+        ))
+    }
+
+    function renderSentSignatures() {
+        return signatureRequests.map((request: any) => (
+            <div className={styles.card}>
+                <h2>{request.name} {request.isSigned && <em>(SIGNED)</em>}</h2>
+                <span><b>Sent to:</b><br />{request.signer}</span><br/>
+                <span><b>Copy:</b><br />{request.content}</span>
+            </div>
         ))
     }
     return (
     <div className={styles.grid}>
-        {renderSignatures()}
+        {isSent ? renderSentSignatures() : renderSignatures()}
     </div>
   )
 }
--- dappreact/utils/actions.ts
+++ dappreact/utils/actions.ts
@@ -21,6 +21,27 @@ export async function fetchSignatureRequests(web3: any) {
             // Verify that signer is same user that is currently signed in
             // Otherwise, do not append to signatures array
             if (signature.signer === web3.account[0]) {
+                console.log(signature);
+                signatures.push({...signature, id: i});
+            }   
+        }
+        return signatures;
+    }
+    catch (e) {
+        return 'error'
+    }
+}
+
+export async function fetchSentSignatureRequests(web3: any) {
+    try {
+        const signatures = [];
+        const docAgreement = new web3.eth.Contract(DocAgreementsABI.abi, CONTRACT_ADDRESS);
+        const docIdCount = await docAgreement.methods.docId().call();
+        for (let i=0; i < docIdCount; i++) {
+            const signature = await docAgreement.methods.documents(i).call();
+            // Verify that owner is same user that is currently signed in
+            // Otherwise, do not append to signatures array
+            if (signature.owner === web3.account[0]) {
                 signatures.push({...signature, id: i});
             }   
         }
